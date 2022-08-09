78269ecaee31f1a65a6cd60f52ccc26d6bf4a364
GoldLion1011 <yogirose79@gmail.com>
2022-08-09 13:19:51 -0700

    syntax on add and pint added

--- add.c
+++ add.c
@@ -14,7 +14,7 @@ void _add(stack_t **stack, unsigned int line_cnt)
 
 	if (!stack || !*stack || !(*stack)->next)
 	{
-		fprintf("L<line_number>: can't add, stack too short\n", line_cnt);
+		fprintf(stderr, "L<line_number>: can't add, stack too short\n", line_cnt);
 		exit(EXIT_FAILURE);
 		return;
 	}
