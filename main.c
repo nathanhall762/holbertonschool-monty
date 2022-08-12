2fab216e0080ba6d676c428de9f42285e7393797
nathanhall762 <4943@holbertonstudents.com>
2022-08-12 09:22:13 -0500

    fix: error message for file not found

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ int main(int argc, char **argv)
 
 	if (!fd) /* error if cannot open file - maybe using fprintf */
 	{
-		fprintf(stderr, "Error: Can't open file\n");
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 
